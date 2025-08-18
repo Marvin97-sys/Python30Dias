26bb703caab3a01894cc1ec6ee14ac069eb430df
Marvin97-sys <rodrigo15097@gmail.com>
2025-08-18 17:15:39 -0600

    Dia 1 - Hola_mundo

---
+++ Hola_mundo.py
@@ -0,0 +1,32 @@
+print("Hola mundo\nMi nombre es Rodrigo")
+
+
+numero = 10.56
+
+print(numero)
+print(type(numero))
+
+
+cadena = "Estoy"
+print(cadena)
+print(type(cadena))
+
+valor = False
+print(valor)
+print(type(valor))
+
+
+num1= 10
+num2 = 6.7
+resultado = (num1 + num2) * 10 /6
+
+print("El resultado es: ",resultado)
+
+#tipado numerico. de cualquier tipo de dato a cualquier tipo de dato#
+'''Prueba de comentarios en python '''
+
+valor = 10
+print(valor)
+
+valor = "Alejandro"
+print(valor)
