bc4d49706eaa2629edadcaacb8f72ae65eef2e47
Marvin97-sys <rodrigo15097@gmail.com>
2025-08-19 17:22:14 -0600

    Dia 2 - Programa para calcular el doble de un numero

---
+++ Ejercicio2.py
@@ -0,0 +1,17 @@
+###a = input ("Ingresa un numero: ")
+num1 = int(input ("Ingresa un numero: "))
+num2 = 2
+
+resultado = num1 * num2
+print("El doble del resultado es: ",resultado)
+
+
+## Importante si voy a pedir numeros el piensa o devuelve string. siempre convertirlo a numero
+# si lo ocupo en numero
+
+
+# exponenciasion
+#num1 = 2
+#num2 = 5
+#resultado = num1 ** num2
+#print("El resultado es: ",resultado)
