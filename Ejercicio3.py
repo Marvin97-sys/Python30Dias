945749f67aa59f4090fd4a2678ff516c520c6988
Marvin97-sys <rodrigo15097@gmail.com>
2025-08-20 17:02:09 -0600

    Dia 3 - Programa para calcular area de un triangulo

---
+++ Ejercicio3.py
@@ -0,0 +1,29 @@
+#b = float(input("Digite la base del Triangulo: "))
+#a = float(input("Digite la altura del Triangulo: "))
+
+#resultado = (b*a/2)
+#print(f"El area del Triangulo es: {resultado}")
+
+
+#ejercicio 2
+##((3+5*8)<3 and ((-6/3x4)+2<2)) or (a>b)
+
+##r = float(input("Digite el digito para r: "))
+##t = float(input("Digite el digito para t: "))
+
+##resultado2 = ((3+5*8)<3 and ((-6/3*4)+2<2)) or (r>t)
+##print(f"El resultado es: {resultado2}")
+
+
+##EJERCICIO 3. HACER UN PROGRAMA PARA INTERCAMBIAR EL VALOR DE 2 VARIABLES
+
+## a = 10 --> a = 5
+## b = 5  --> b = 10
+
+a = input("a ->: ")
+b = input("b ->: ")
+
+a , b = b , a
+
+print(f"El nuevo valor de a es: {a}")
+print(f"El nuevo valor de b es: {b}")
