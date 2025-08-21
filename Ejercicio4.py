1e2e18189d7a38fa6fbc723887abb26beb45b445
Marvin97-sys <rodrigo15097@gmail.com>
2025-08-21 16:52:24 -0600

    Actualizo Dia 4 - agrego información

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -6,5 +6,16 @@ b = int(input("Digite un numero2: "))
 
 if a > b:
     print(f"El numero mayor es {a}")
+elif a == b:
+    print(f"Los numeros son iguales")
 else:
     print(f"El numero mayor es {b}")
+
+
+#•	Día 5: Pedir una edad y decir si es mayor de edad (≥18).
+
+edad = int(input("Digite su edad: "))
+if edad >= 18:
+    print(f"Usted es mayor de edad")
+else:
+    print(f"Usted no es mayor de edad")
