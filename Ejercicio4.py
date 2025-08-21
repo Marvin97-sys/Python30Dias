799eff3afc4aa85d08bc1d9efd8282d7efd9c8d5
Marvin97-sys <rodrigo15097@gmail.com>
2025-08-21 16:45:27 -0600

    Dia 4 - Programa para calcular el numero mayor

---
+++ Ejercicio4.py
@@ -0,0 +1,10 @@
+##•	Día 4: Pedir dos números y mostrar el mayor.
+
+a = int(input("Digite un numero1: "))
+b = int(input("Digite un numero2: "))
+
+
+if a > b:
+    print(f"El numero mayor es {a}")
+else:
+    print(f"El numero mayor es {b}")
